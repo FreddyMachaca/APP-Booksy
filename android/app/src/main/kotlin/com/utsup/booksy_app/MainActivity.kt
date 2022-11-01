98fbd5e5bb2a9a454064174289035ded2ee1149e
freddy <freddyymachaca@gmail.com>
2022-10-31 23:03:17 -0400

    Bottom Navigation Bar

---
+++ android/app/src/main/kotlin/com/utsup/booksy_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.utsup.booksy_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Booksy',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: const BottomNavigationWidget(),
+    );
+  }
+}
+
+class BottomNavigationWidget extends StatefulWidget {
+  const BottomNavigationWidget({super.key});
+
+  @override
+  State<BottomNavigationWidget> createState() => _BottomNavigationWidgetState();
+}
+
+class _BottomNavigationWidgetState extends State<BottomNavigationWidget> {
+  int _selectIndex = 0;
+
+  static const List<Widget> _sections=[Text("inicio"),Text("biblioteca")];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Booksy'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Inicio',
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.local_library),
+              label: 'Biblioteca'
+          ),
+        ],
+        currentIndex: _selectIndex,
+        onTap: _onItemTapped,
+      ),
+      body: _sections[_selectIndex],
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectIndex = index;
+    });
+  }
+}
